0f04d466db3aa75b38d2afba6da1c283801ea876
knicolosi313 <knicolosi313@gmail.com>
2026-04-13 18:23:50 -0400

    fix

--- src/backend/mcp_servers/mcp_formatting/test.py
+++ src/backend/mcp_servers/mcp_formatting/test.py
@@ -1,11 +1,9 @@
-from image_loader import *
+
 from pdf_converter import *
 import asyncio
 from test_formatting_server import *
 
 async def main():
-    # port = start_image_server(8000)
-    # prepare_image_urls(IMAGES)
     with open("latest_report.html", encoding="utf-8") as file:
         content = file.read()
     await html_to_pdf(content)
